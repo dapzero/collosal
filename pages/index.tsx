cacd3b0c9c67e95ce6bca914778c0550f52b0b5c
Muadib Ibrahim <93816890+dapzero@users.noreply.github.com>
2026-04-15 11:39:55 +0700

    feat: update NavBar contact link to localized version and modify Home section titles for better clarity
    /pages/index.tsx
    - Remove How we work, Our Teams. Prices

--- pages/index.tsx
+++ pages/index.tsx
@@ -85,56 +85,6 @@ const Home = () => {
         </section>
         <LogoList />
 
-        {/* How We Work */}
-        <section className="grid grid-cols-1 gap-6 sm:gap-8 place-items-center lg:grid-cols-2">
-          <aside className="w-full h-[400px] relative" data-aos="fade-right">
-            <img
-              src={'/images/how-we-work-illustration.svg'}
-              style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-              alt="Structured plan"
-            />
-          </aside>
-          <aside
-            className="text-center sm:w-10/12 lg:text-left lg:w-full"
-            data-aos="fade-left"
-          >
-            <SectionSentence
-              title="Everything is well planned, well designed and developed wholeheartedly"
-              paragraph="Careful planning makes us confident, developed with best practices so that the project can be maintained. We always test projects before they are shipped."
-              badge="HOW WE WORK"
-            />
-          </aside>
-        </section>
-        {/* Our Teams */}
-        <section className="grid grid-cols-1 gap-6 sm:gap-8 place-items-center lg:grid-cols-2">
-          <aside className="text-center sm:w-10/12 lg:text-left lg:w-full">
-            <div className="space-y-12" data-aos="fade-right">
-              <SectionSentence
-                title="We're a team of designers, engineers and analysts"
-                paragraph="Our team consists of many creative people. We are committed to maintaining quality work as well as speed. These creative people work together to create maximum work results."
-                badge="OUR TEAM"
-              />
-              <ButtonLink
-                value="See Our Teams"
-                href="/teams"
-                size="small"
-                color="white"
-                style="light"
-              />
-            </div>
-          </aside>
-          <aside
-            className="w-full h-[400px] relative sm:w-8/12 lg:w-full"
-            data-aos="fade-left"
-          >
-            <img
-              src={'/images/team-illustration.svg'}
-              style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-              alt="Code editor and UI Editing popup"
-            />
-          </aside>
-        </section>
-        <LineDivider />
         {/* Our Projects */}
         <section className="flex flex-col gap-16 items-center">
           <div
@@ -142,7 +92,7 @@ const Home = () => {
             data-aos="zoom-in-up"
           >
             <SectionSentence
-              title="Kami telah menyelesaikan banyak proyek yang menakjubkan yang tidak akan Anda percayai"
+              title="Proyek terbaru kami, kami yakin Anda akan menyukainya."
               badge="PROYEK KAMI"
             />
           </div>
@@ -151,6 +101,8 @@ const Home = () => {
               <ProjectCard
                 title="The Desktop App Landing Page"
                 description="A landing page for desktop app"
+                bottomSquareSize="big"
+                imageSrc='/images/ui-design-illustration.svg'
               />
             </div>
             <div data-aos="flip-right">
@@ -158,70 +110,18 @@ const Home = () => {
                 title="The Mobile App Landing Page"
                 description="A landing page for mobile app"
                 bottomSquareSize="big"
+                imageSrc='/images/ui-design-illustration.svg'
               />
             </div>
           </div>
           <ButtonLink value="Lihat Semua Proyek" href='/project' color="white" style="light" />
         </section>
-        {/* Prices */}
-        <section className="flex flex-col gap-16 items-center">
-          <div
-            className="text-center sm:w-10/12 md:w-8/12 lg:w-6/12"
-            data-aos="zoom-in-up"
-          >
-            <SectionSentence
-              title="What do you need? Choose a service that can help you"
-              badge="GET STARTED"
-            />
-          </div>
-          <div className="w-full grid grid-cols-1 gap-10 sm:grid-cols-2 sm:gap-5 lg:grid-cols-3">
-            <div data-aos="fade-up-right">
-              <PricingCard
-                price="1200$"
-                title="UI Design"
-                href='/contact'
-                features={[
-                  '10 design pages',
-                  'Well-documented',
-                  '4 revisions',
-                  '$100/additional page',
-                ]}
-              />
-            </div>
-            <div data-aos="fade-up" data-aos-delay="300">
-              <PricingCard
-                price="5000$"
-                title="Development"
-                href='/contact'
-                features={[
-                  'Web & Mobile',
-                  'Well-documented',
-                  '8 revisions',
-                  '$1000/additional page',
-                ]}
-              />
-            </div>
-            <div data-aos="fade-up-left">
-              <PricingCard
-                price="3000$"
-                title="Maintenance"
-                href='contact'
-                features={[
-                  'Daily backup',
-                  '3 hours of maintenance',
-                  'Including fixing',
-                  '$50/additional hour',
-                ]}
-              />
-            </div>
-          </div>
-        </section>
         {/* Testimonial */}
         <section className="flex flex-col gap-16 items-center">
           <div className="text-center sm:w-10/12 md:w-8/12 lg:w-6/12" data-aos="zoom-in-up">
             <SectionSentence
-              title="What do our clients say that we never let down?"
-              badge="TESTIMONIAL"
+              title="Apa kata mereka terhadap kami?"
+              badge="TESTIMONI"
             />
           </div>
           <div className="w-full" data-aos="fade-up">
